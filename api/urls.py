688b6a3a9108992a933587dbbaf4ea403683b3a8
PTHARRISH <harrishpt@gmail.com>
2025-12-20 13:11:57 +0530

    Day 10: New apps Classes Imported

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-from api.views import (
-    AssignUserPermissionView,
+from rbac.views import AssignUserPermissionView
+from users.views import (
     BlockUserView,
     CloseFriendView,
     DeleteAccountView,
